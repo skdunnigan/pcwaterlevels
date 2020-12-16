535c496a17ebd1b1531793747e5acd784bdd0533
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2020-12-16 17:06:49 -0500

    added addition code for wrangling

--- R/01_load_wrangle.R
+++ R/01_load_wrangle.R
@@ -11,6 +11,26 @@ pc_usgs_2 <- readxl::read_xlsx(here::here('data', 'bings_070119_010120.xlsx')) %
 tail(pc_usgs_1)
 head(pc_usgs_2)
 
-pc_usgs <- bind_rows(pc_usgs_1, pc_usgs_2)
+pc_usgs <- bind_rows(pc_usgs_1, pc_usgs_2) %>% 
+  dplyr::select(date_time_edt, navd88_water_level_m) %>% 
+  dplyr::mutate(level = as.numeric(navd88_water_level_m)) %>% 
+  dplyr::rename(datetime = date_time_edt) %>% 
+  dplyr::select(-navd88_water_level_m)
 
 rm(pc_usgs_1, pc_usgs_2) # remove chunks
+
+a <- pc_usgs %>% 
+  ggplot(aes(x = datetime, y = level)
+
+ggplotly(a)
+
+
+# SWMP
+
+pc <- readr::read_csv(here::here('data', 'gtmpcwq2019.csv')) %>% 
+  janitor::clean_names()
+
+
+
+
+
